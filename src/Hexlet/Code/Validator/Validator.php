b4de8b8962128362c53d55e0f16c29a614747b08
Andre <ymottnaf@yandex.ru>
2024-11-12 20:24:14 +0300

    add_array

--- src/Hexlet/Code/Validator/Validator.php
+++ src/Hexlet/Code/Validator/Validator.php
@@ -4,72 +4,92 @@ namespace Hexlet\Validator;
 
 class Validator
 {
-    private $required, $minLength, $contains, $type, $isPositive, $range;
+    private $required, $minLength, $contains, $type, $isPositive, $range, $sizeof;
 
-    function __construct()
+    function __construct($type = false)
     {
-        $this->type = false;
+        $this->type = $type;
         $this->required  = false;
         $this->minLength = false;
         $this->contains = false;
         $this->isPositive = false;
-        $this->range = array();
+        $this->range = false;
+        $this->sizeof = false;
     }
+
     function string()
     {
-        $this->type = 'string';
-        return new Validator();
+        return new Validator('string');
     }
 
     function number()
     {
-        $this->type = 'number';
-        return new Validator();
+        return new Validator('number');
+    }
+
+    function array()
+    {
+        return new Validator('array');
     }
 
     function isValid($request)
     {
         $isValid = false;
-        if ($this->required || (!$this->required && ($request === null || is_string($request)))) {
-            $isValid = true;
-        } else {
-            $isValid = false;
+        switch ($this->type)
+        {
+            case 'string':
+                $isValid = ($this->minLength === false || ($this->minLength && is_string($request) && strlen($request) >= $this->minLength))
+                    && ($this->contains === false || ($this->contains && is_string($request) && strpos($request, $this->contains) !== false))
+                    && ((($request === null || $request == '') && !$this->required) || $request) ? true : false;
+                break;
+            case 'number':
+                $isValid = (($request === null && !$this->required) || $request !== null)
+                    && (($this->isPositive && $request > 0) || !$this->isPositive)
+                    && (($this->range && $request >= $this->range[0] && $request <= $this->range[1]) || $this->range === false) ? true : false;
+                break;
+            case 'array':
+                $isValid = (($request === null && !$this->required) || is_array($request))
+                    && ($this->sizeof === false || ($this->sizeof && is_array($request) && count($request) == $this->sizeof)) ? true : false;
+                break;
         }
-        if ($isValid && $this->minLength && ($request === null || is_string($request) || strlen($request) < $this->minLength)) {
-            $isValid = false;
-        }
-        if ($isValid && $this->contains && ($request === null || is_string($request) || strpos($request, $this->contains) === false)) {
-            $isValid = false;
-        }
-        $this->minLength = false;
-        $this->contains = false;
-        $this->isPositive = false;
-        $this->range = array();
         return $isValid;
     }
 
     function required()
     {
         $this->required = true;
+        if ($this->type == 'array') {
+            return $this;
+        }
     }
 
     function contains(string $request)
     {
         $this->contains = $request;
+        return $this;
     }
 
     function minLength(int $request)
     {
         $this->minLength = $request;
+        return $this;
     }
 
     function positive()
     {
         $this->isPositive = true;
+        return $this;
     }
 
     function range(int $a, int $b)
     {
         $this->range = [$a, $b];
+        return $this;
+    }
+
+    function sizeof(int $request)
+    {
+        $this->sizeof = $request;
+        return true;
     }
 }
